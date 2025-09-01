aad6900d0681be5b0d3e9d6c7c2b8b445a580e1e
Muhammad Haris <muhxmmadharis@Muhammads-MacBook-Pro.local>
2025-09-01 19:34:02 +0100

    Adding history of trades and exit functionality

--- trading_journal.py
+++ trading_journal.py
@@ -1,5 +1,6 @@
 journal_running = True
 portfolio={}
+trade_history=[]
 
 print("\nWelcome to My Trading Journal")
 print("-----------------------------")
@@ -11,12 +12,23 @@ while journal_running==True:
         stock_sticker=input("Enter Stock Sticker: ")
         number_of_shares= int(input("Enter number of Shares you want to buy: "))
         buy_price_per_share=int(input("Enter buy price per share: "))
+        price_of_trade= buy_price_per_share*number_of_shares
+        new_trade={
+                "Stock":stock_sticker,
+                "Shares":number_of_shares,
+                "Price":price_of_trade
+            }
+        trade_history.append(new_trade)
+
         if stock_sticker in portfolio:
             total_shares=portfolio[stock_sticker]["Total Shares"] + number_of_shares
             total_investement=portfolio[stock_sticker]["Total Investement"] + number_of_shares*buy_price_per_share
             portfolio[stock_sticker]["Total Shares"]=total_shares
             portfolio[stock_sticker]["Total Investement"]=total_investement
             portfolio[stock_sticker]["Average Price"]=total_investement/total_shares
+           
+
+
        
         
         else:
@@ -32,6 +44,20 @@ while journal_running==True:
         for stock_sticker, value  in portfolio.items():
             print(f"{stock_sticker}:Total Shares:{value['Total Shares']}, Total Investement: ${value['Total Investement']:.2f}, Average Price: ${value['Average Price']:.2f}")
             print("\n")
+    
+    elif choose_option==3:
+        print("📝 Trade History")
+        print("-----------------")
+        for x in trade_history:
+            print(f"You bought {x['Shares']} of {x['Stock']} for ${x['Price']:.2f}" )
+
+    elif choose_option==4:
+        print("👋 Exiting Trading Journal. Goodbye!")
+        journal_running=False
+        
+    else:
+        print("Incorrect Input. Please enter the Number corresponding to your needs.")
+    
 
 
 
